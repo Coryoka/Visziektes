7f1b80a22e16fe506581abd639fc9c5347d5b062
Donny Olijsager <d.olijslager@student.han.nl>
2017-05-11 15:03:27 +0200

    eerste update

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,30 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+    @FXML Button homeButton;
+    @FXML Text welcomeText;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        homeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if(welcomeText.getText().equals("text")){
+                    welcomeText.setText("welcome");
+                } else {
+                    welcomeText.setText("text");
+                }
+            }
+        });
+    }
 }
